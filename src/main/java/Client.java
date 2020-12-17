a0a697aac78b9f0fb1bf1c72c37f9e0a6542211a
greedo85 <qiliveadata@gmail.com>
2020-12-17 17:34:57 +0100

    #1 implementacja metod

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,2 +1,27 @@
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
+
+    private Socket socket;
+    private OutputStream outputStream;
+    private BufferedReader bufferedReader;
+    private String read;
+    private String write;
+    private String name;
+
+    public Client() throws IOException {
+        socket= new Socket("localhost", 888);
+        outputStream= socket.getOutputStream();
+        bufferedReader= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public void read() throws IOException {
+        read= bufferedReader.readLine();
+    }
+
+    public void write(String text) throws IOException {
+        write=text;
+        outputStream.write((name+":"+write).getBytes());
+    }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private OutputStream outputStream;
+    private String read;
+    private String write;
+
+
+    public Server(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+        this.socket = serverSocket.accept();
+        this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.outputStream = socket.getOutputStream();
+    }
+
+    public void read() throws IOException {
+        read= bufferedReader.readLine();
+    }
+
+    public void send(String text) throws IOException {
+        write=text;
+        outputStream.write(text.getBytes());
+    }
 }
